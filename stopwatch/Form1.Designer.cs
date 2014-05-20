4c873e5426ccd50526b01c8a83b24f06c4d8b360
Ivan Popov <unfragable@data.bg>
2014-05-20 20:04:18 +0300

    visually ok but there is still some coding

---
+++ stopwatch/Form1.Designer.cs
@@ -0,0 +1,270 @@
+﻿namespace stopwatch
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.startBtn = new System.Windows.Forms.Button();
+            this.hoursLbl = new System.Windows.Forms.Label();
+            this.minutesLbl = new System.Windows.Forms.Label();
+            this.secondsLbl = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btn1 = new System.Windows.Forms.Button();
+            this.btn5 = new System.Windows.Forms.Button();
+            this.btn4 = new System.Windows.Forms.Button();
+            this.btn6 = new System.Windows.Forms.Button();
+            this.btn7 = new System.Windows.Forms.Button();
+            this.btn0 = new System.Windows.Forms.Button();
+            this.btn9 = new System.Windows.Forms.Button();
+            this.btn8 = new System.Windows.Forms.Button();
+            this.btn3 = new System.Windows.Forms.Button();
+            this.btn2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // startBtn
+            // 
+            this.startBtn.Location = new System.Drawing.Point(81, 249);
+            this.startBtn.Name = "startBtn";
+            this.startBtn.Size = new System.Drawing.Size(109, 40);
+            this.startBtn.TabIndex = 0;
+            this.startBtn.Text = "Start";
+            this.startBtn.UseVisualStyleBackColor = true;
+            this.startBtn.Click += new System.EventHandler(this.startBtn_Click);
+            // 
+            // hoursLbl
+            // 
+            this.hoursLbl.AutoSize = true;
+            this.hoursLbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.hoursLbl.Location = new System.Drawing.Point(2, 13);
+            this.hoursLbl.Name = "hoursLbl";
+            this.hoursLbl.Size = new System.Drawing.Size(80, 55);
+            this.hoursLbl.TabIndex = 1;
+            this.hoursLbl.Text = "00";
+            // 
+            // minutesLbl
+            // 
+            this.minutesLbl.AutoSize = true;
+            this.minutesLbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.minutesLbl.Location = new System.Drawing.Point(88, 13);
+            this.minutesLbl.Name = "minutesLbl";
+            this.minutesLbl.Size = new System.Drawing.Size(80, 55);
+            this.minutesLbl.TabIndex = 2;
+            this.minutesLbl.Text = "00";
+            // 
+            // secondsLbl
+            // 
+            this.secondsLbl.AutoSize = true;
+            this.secondsLbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.secondsLbl.Location = new System.Drawing.Point(174, 13);
+            this.secondsLbl.Name = "secondsLbl";
+            this.secondsLbl.Size = new System.Drawing.Size(80, 55);
+            this.secondsLbl.TabIndex = 3;
+            this.secondsLbl.Text = "00";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(66, 46);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(16, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "H";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(152, 46);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(17, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "M";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(238, 46);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(15, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "S";
+            // 
+            // btn1
+            // 
+            this.btn1.Location = new System.Drawing.Point(81, 83);
+            this.btn1.Name = "btn1";
+            this.btn1.Size = new System.Drawing.Size(29, 27);
+            this.btn1.TabIndex = 7;
+            this.btn1.Text = "1";
+            this.btn1.UseVisualStyleBackColor = true;
+            this.btn1.Click += new System.EventHandler(this.btn1_Click);
+            // 
+            // btn5
+            // 
+            this.btn5.Location = new System.Drawing.Point(122, 116);
+            this.btn5.Name = "btn5";
+            this.btn5.Size = new System.Drawing.Size(29, 27);
+            this.btn5.TabIndex = 8;
+            this.btn5.Text = "5";
+            this.btn5.UseVisualStyleBackColor = true;
+            this.btn5.Click += new System.EventHandler(this.btn5_Click);
+            // 
+            // btn4
+            // 
+            this.btn4.Location = new System.Drawing.Point(81, 116);
+            this.btn4.Name = "btn4";
+            this.btn4.Size = new System.Drawing.Size(29, 27);
+            this.btn4.TabIndex = 9;
+            this.btn4.Text = "4";
+            this.btn4.UseVisualStyleBackColor = true;
+            this.btn4.Click += new System.EventHandler(this.btn4_Click);
+            // 
+            // btn6
+            // 
+            this.btn6.Location = new System.Drawing.Point(161, 116);
+            this.btn6.Name = "btn6";
+            this.btn6.Size = new System.Drawing.Size(29, 27);
+            this.btn6.TabIndex = 10;
+            this.btn6.Text = "6";
+            this.btn6.UseVisualStyleBackColor = true;
+            this.btn6.Click += new System.EventHandler(this.btn6_Click);
+            // 
+            // btn7
+            // 
+            this.btn7.Location = new System.Drawing.Point(81, 149);
+            this.btn7.Name = "btn7";
+            this.btn7.Size = new System.Drawing.Size(29, 27);
+            this.btn7.TabIndex = 11;
+            this.btn7.Text = "7";
+            this.btn7.UseVisualStyleBackColor = true;
+            this.btn7.Click += new System.EventHandler(this.btn7_Click);
+            // 
+            // btn0
+            // 
+            this.btn0.Location = new System.Drawing.Point(122, 182);
+            this.btn0.Name = "btn0";
+            this.btn0.Size = new System.Drawing.Size(29, 27);
+            this.btn0.TabIndex = 12;
+            this.btn0.Text = "0";
+            this.btn0.UseVisualStyleBackColor = true;
+            this.btn0.Click += new System.EventHandler(this.btn0_Click);
+            // 
+            // btn9
+            // 
+            this.btn9.Location = new System.Drawing.Point(161, 149);
+            this.btn9.Name = "btn9";
+            this.btn9.Size = new System.Drawing.Size(29, 27);
+            this.btn9.TabIndex = 13;
+            this.btn9.Text = "9";
+            this.btn9.UseVisualStyleBackColor = true;
+            this.btn9.Click += new System.EventHandler(this.btn9_Click);
+            // 
+            // btn8
+            // 
+            this.btn8.Location = new System.Drawing.Point(122, 149);
+            this.btn8.Name = "btn8";
+            this.btn8.Size = new System.Drawing.Size(29, 27);
+            this.btn8.TabIndex = 14;
+            this.btn8.Text = "8";
+            this.btn8.UseVisualStyleBackColor = true;
+            this.btn8.Click += new System.EventHandler(this.btn8_Click);
+            // 
+            // btn3
+            // 
+            this.btn3.Location = new System.Drawing.Point(161, 83);
+            this.btn3.Name = "btn3";
+            this.btn3.Size = new System.Drawing.Size(29, 27);
+            this.btn3.TabIndex = 15;
+            this.btn3.Text = "3";
+            this.btn3.UseVisualStyleBackColor = true;
+            this.btn3.Click += new System.EventHandler(this.btn3_Click);
+            // 
+            // btn2
+            // 
+            this.btn2.Location = new System.Drawing.Point(122, 83);
+            this.btn2.Name = "btn2";
+            this.btn2.Size = new System.Drawing.Size(29, 27);
+            this.btn2.TabIndex = 16;
+            this.btn2.Text = "2";
+            this.btn2.UseVisualStyleBackColor = true;
+            this.btn2.Click += new System.EventHandler(this.btn2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(271, 301);
+            this.Controls.Add(this.btn2);
+            this.Controls.Add(this.btn3);
+            this.Controls.Add(this.btn8);
+            this.Controls.Add(this.btn9);
+            this.Controls.Add(this.btn0);
+            this.Controls.Add(this.btn7);
+            this.Controls.Add(this.btn6);
+            this.Controls.Add(this.btn4);
+            this.Controls.Add(this.btn5);
+            this.Controls.Add(this.btn1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.secondsLbl);
+            this.Controls.Add(this.minutesLbl);
+            this.Controls.Add(this.hoursLbl);
+            this.Controls.Add(this.startBtn);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button startBtn;
+        private System.Windows.Forms.Label hoursLbl;
+        private System.Windows.Forms.Label minutesLbl;
+        private System.Windows.Forms.Label secondsLbl;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btn1;
+        private System.Windows.Forms.Button btn5;
+        private System.Windows.Forms.Button btn4;
+        private System.Windows.Forms.Button btn6;
+        private System.Windows.Forms.Button btn7;
+        private System.Windows.Forms.Button btn0;
+        private System.Windows.Forms.Button btn9;
+        private System.Windows.Forms.Button btn8;
+        private System.Windows.Forms.Button btn3;
+        private System.Windows.Forms.Button btn2;
+    }
+}
+
---
+++ stopwatch/Form1.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace stopwatch
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        int hours = 0, minutes = 0, seconds = 0;       
+        int[,] timeArr = { { 0,0 }, { 0,0 }, { 0,0 } };
+        Boolean startTime = false;
+
+        public void shift(int number)
+        {
+            timeArr[2, 1] = timeArr[2, 0];
+            timeArr[2, 0] = timeArr[1, 1];
+            timeArr[1, 1] = timeArr[1, 0];
+            timeArr[1, 0] = timeArr[0, 1];
+            timeArr[0, 1] = timeArr[0, 0];
+            timeArr[0, 0] = number;
+
+            update();
+        }
+
+        public void update()
+        {
+            secondsLbl.Text = System.Convert.ToString(timeArr[0, 1]) + System.Convert.ToString(timeArr[0, 0]);
+            minutesLbl.Text = System.Convert.ToString(timeArr[1, 1]) + System.Convert.ToString(timeArr[1, 0]);
+            hoursLbl.Text = System.Convert.ToString(timeArr[2, 1]) + System.Convert.ToString(timeArr[2, 0]);
+        }
+
+        public void updateTime()
+        {
+            secondsLbl.Text = System.Convert.ToString(seconds);
+            minutesLbl.Text = System.Convert.ToString(minutes);
+            hoursLbl.Text = System.Convert.ToString(hours);
+        }
+
+        public void startTimer()
+        {            
+            seconds--;
+            updateTime();
+            System.Threading.Thread.Sleep(4000);            
+        }
+
+        private void startBtn_Click(object sender, EventArgs e)
+        {
+            btn1.Enabled = false;
+            btn2.Enabled = false;
+            btn3.Enabled = false;
+            btn4.Enabled = false;
+            btn5.Enabled = false;
+            btn6.Enabled = false;
+            btn7.Enabled = false;
+            btn8.Enabled = false;
+            btn9.Enabled = false;
+            btn0.Enabled = false;
+            startBtn.Enabled = false;
+
+            seconds = System.Convert.ToInt32(secondsLbl.Text);
+            minutes = System.Convert.ToInt32(minutesLbl.Text);
+            hours = System.Convert.ToInt32(hoursLbl.Text);
+
+            if (seconds > 59)
+            {
+                minutes++;
+                seconds -= 59;
+            }
+
+            if (minutes > 59)
+            {
+                hours++;
+                minutes -= 59;
+            }
+
+            if (hours == 0)
+            {
+                hoursLbl.Visible = false;
+                label1.Visible = false;
+            }
+
+            if (hours < 10)
+            {
+                hoursLbl.Location = new Point(31, 13);
+            }
+
+            if (seconds < 10)
+            {
+                secondsLbl.Text = "0" + System.Convert.ToString(seconds);
+            }
+            else
+            {
+                secondsLbl.Text = System.Convert.ToString(seconds);
+            }
+
+            minutesLbl.Text = System.Convert.ToString(minutes);
+            hoursLbl.Text = System.Convert.ToString(hours);
+
+            
+        }
+
+        private void btn1_Click(object sender, EventArgs e)
+        {
+            shift(1);
+        }
+
+        private void btn0_Click(object sender, EventArgs e)
+        {
+            shift(0);
+        }
+
+        private void btn2_Click(object sender, EventArgs e)
+        {
+            shift(2);
+        }
+
+        private void btn3_Click(object sender, EventArgs e)
+        {
+            shift(3);
+        }
+
+        private void btn4_Click(object sender, EventArgs e)
+        {
+            shift(4);
+        }
+
+        private void btn5_Click(object sender, EventArgs e)
+        {
+            shift(5);
+        }
+
+        private void btn6_Click(object sender, EventArgs e)
+        {
+            shift(6);
+        }
+
+        private void btn7_Click(object sender, EventArgs e)
+        {
+            shift(7);
+        }
+
+        private void btn8_Click(object sender, EventArgs e)
+        {
+            shift(8);
+        }
+
+        private void btn9_Click(object sender, EventArgs e)
+        {
+            shift(9);
+        }
+    }
+}
